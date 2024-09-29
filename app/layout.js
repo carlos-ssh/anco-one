ccd30f4aac17d0d3614b7d6bba23cf99534306cf
Omar <omar@Omars-MacBook-Pro.local>
2024-09-28 19:12:50 -0600

    Add main configurationz

---
+++ app/layout.js
@@ -0,0 +1,30 @@
+import localFont from "next/font/local";
+import NavbarComponent from "@/components/header/NavbarComponent";
+import "./globals.css";
+
+const anconeDisplay = localFont({
+  src: "./fonts/AnconeDisplay.ttf",
+  variable: "--font-ancone-display",
+  weight: "100 900",
+});
+const anconeText = localFont({
+  src: "./fonts/ancone.ttf",
+  variable: "--font-ancone-regular",
+  weight: "100 900",
+});
+export const metadata = {
+  title: "anco one",
+  description: "las verdades ocultas tras pantallas bien pulidas",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="es-MX">
+      <body
+        className={`${anconeDisplay.variable} ${anconeText.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
